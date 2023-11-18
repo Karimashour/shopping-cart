7c5a111ef2df1d759fa8254727c471b941739097
Karimashour <77418051+Karimashour@users.noreply.github.com>
2023-11-18 13:47:55 +0200

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Home.css"
+const About = () => {
+  return (
+    <div className="about">
+      <Container>
+        <h1  style={{ textAlign: "center", color: "black" }}> About Us</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="homee">
+      <Container>
+        <h1 style={{ textAlign: "center", color: "black" }}>
+          Welcome in our store
+        </h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
